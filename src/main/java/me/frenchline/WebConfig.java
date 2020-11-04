5adf4bedb7f9cdedf386141b080f688dc2387e41
frenchline <frenchline707@gmail.com>
2020-11-04 16:09:54 +0900

    스프링 MVC 연동, 서블릿 구현체 DispatcherServlet을 사용해서 웹 애플리케이션 컨텍스트를 만들고 서블릿에서 Front Controller 역할로 사용하는 방법
    DispatcherServlet 계층 없음, 애플리케이션 컨텍스트 하나만 사용
    Servlet WebApplicationContext : All Beans (DispatcherServlet이 만든 Application Context)
    
    - WebConfig 라는 설정파일 하나만 가지고 모든 빈을 등록
    - web.xml 에서 ContextLoaderListener를 더이상 사용하지 않음
    
    -----
    서블릿 애플리케이션에 스프링 연동하기
    ● 스프링이 제공하는 서블릿 구현체 DispatcherServlet 사용하기
    
    DispatcherServlet
    ● 스프링 MVC의 핵심.
    ● Front Controller 역할을 한다

--- src/main/java/me/frenchline/WebConfig.java
+++ src/main/java/me/frenchline/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
  * @since 2019-11-04
  */
 @Configuration
-@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class)) //디폴트 필터들을 사용하지 않고, 오직 컨트롤러만 빈으로 등록하겠다
-public class WebConfig { //자식 애플리케이션 컨텍스트(Servlet WebApplicationContext)
+@ComponentScan
+public class WebConfig {
 
 }
