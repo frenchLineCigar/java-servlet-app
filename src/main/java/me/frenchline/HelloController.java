30b3f8435236d6f4ff50c5bfb935f0311fccbaaa
frenchline <frenchline707@gmail.com>
2020-11-04 20:38:51 +0900

    DispatcherServlet 동작 원리 1, 2

--- src/main/java/me/frenchline/HelloController.java
+++ src/main/java/me/frenchline/HelloController.java
@@ -1,22 +1,33 @@
 package me.frenchline;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author swlee
  * @contact frenchline707@gmail.com
  * @since 2019-11-04
+ *
+ *  DispatcherServlet의 기본 핸들러 맵핑 전략 중 RequestMappingHandlerMapping을 사용하는 방법
+ *  핸들러 실행: RequestMappingHandlerAdapter -> 애노테이션 기반(@GetMapping 등)의 스프링 MVC 핸들러를 실행하는 핸들러 어댑터
  */
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     HelloService helloService;
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "Hello, " + helloService.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "/WEB-INF/sample.jsp"; //ModelAndView [view="/WEB-INF/sample.jsp"; model={}]
     }
 }
---
+++ src/main/java/me/frenchline/SimpleController.java
@@ -0,0 +1,24 @@
+package me.frenchline;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author swlee
+ * @contact frenchline707@gmail.com
+ * @since 2019-11-04
+ *
+ * DispatcherServlet의 기본 핸들러 맵핑 전략 중 BeanNameUrlHandlerMapping을 사용하는 방법
+ * 핸들러 실행: SimpleControllerHandlerAdapter -> Controller 인터페이스를 구현해서 만든 핸들러를 실행하는 핸들러 어댑터
+ */
+@org.springframework.stereotype.Controller("/simple") //BeanNameUrl을 설정: /simple 이라는 요청을 처리하는 핸들러가 된다
+public class SimpleController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return new ModelAndView("/WEB-INF/simple.jsp");
+    }
+}
